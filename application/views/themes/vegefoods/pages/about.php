62935f9807c8e2e72d91068dc27aa4fc29d70414
dfsabrina <dfsabrina@upi.edu>
2020-06-27 18:25:19 +0700

    First commit

---
+++ application/views/themes/vegefoods/pages/about.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<div class="hero-wrap hero-bread" style="background-image: url('<?php echo get_theme_uri('images/bg_1.jpg'); ?>');">
+    <div class="container">
+      <div class="row no-gutters slider-text align-items-center justify-content-center">
+        <div class="col-md-9 ftco-animate text-center">
+            <p class="breadcrumbs"><span class="mr-2"><?php echo anchor(base_url(), 'Home'); ?></span> <span>Tentang Kami</span></p>
+          <h1 class="mb-0 bread">Tentang Kami</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="ftco-section ftco-no-pb ftco-no-pt bg-light">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url(<?php echo get_theme_uri('images/about.jpg'); ?>);">
+                      <a href="https://vimeo.com/45830194" class="icon popup-vimeo d-flex justify-content-center align-items-center">
+                          <span class="icon-play"></span>
+                      </a>
+                  </div>
+                  <div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+            <div class="heading-section-bold mb-4 mt-md-5">
+                <div class="ml-md-0">
+                  <h2 class="mb-4">Selamat Datang di Toko Online <?php echo get_store_name(); ?></h2>
+              </div>
+            </div>
+           
+                  </div>
+              </div>
+          </div>
+      </section>
+      
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  </div>
+      </div>
+  </section>
